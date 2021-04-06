1916415f9f164a962252915a58b7e8eaf460acb9
Lev Karavashkin <kolt.zar@mail.ru>
2021-04-06 19:52:40 +0300

    clear template

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const io = require('socket.io')(server,{
 });
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', (socket) => {
 
   console.log('User connected')
@@ -23,20 +19,7 @@ io.on('connection', (socket) => {
   // присоединяемся к комнате (входим в нее)
   socket.join(roomId)
 
-  // io.in(socket.roomId).emit('play', {who: 'ya', isPlaying: false});
-  // io.in(socket.roomId).emit('next', 'ya');
-  // io.in(socket.roomId).emit('prev', 'ya');
-
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-
-  socket.on('play',who => io.in(socket.roomId).emit('play',who));
-  socket.on('pause',who => io.in(socket.roomId).emit('pause',who));
-  socket.on('next',who => io.in(socket.roomId).emit('next',who));
-  socket.on('prev',who => io.in(socket.roomId).emit('prev',who));
-  socket.on('time',({who,time}) => io.in(socket.roomId).emit('time',{who,time}));
-  socket.on('setAudio',({who,audio}) => io.in(socket.roomId).emit('setAudio',{who,audio}));
+  socket.on('sync', (who,data) => io.in(socket.roomId).emit('sync',{who,data}))
 
   // обрабатываем отключение сокета-пользователя
   socket.on('disconnect', () => {
